540de5895609b1ea8a26a93f6886cbdf37e06b1b
Niraj Thing <linkinson7@gmail.com>
2019-07-06 22:57:05 +0545

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/',views.signup, name='signup'),
+    path('login/',views.login, name='login'),
+    path('logout/',views.logout, name='logout'),
+    path('contact-us/', views.contactus, name="contactus"),
+    path('about-us/', views.aboutus, name="aboutus"),
+    path('home/',views.home,name='home'),
+]
